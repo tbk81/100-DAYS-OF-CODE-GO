8a6041c9320b788d738e8cdd2013bd1b51de21a9
tbk81 <dev@tbk81.com>
2024-10-27 05:45:33 -0700

    claude code

--- 1-band-name-gen/main.go
+++ 1-band-name-gen/main.go
@@ -1,13 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Welcome to Band Name Generator.")
 	var city, pet string
 	fmt.Println("What's the name of the city you grew up in?")
-	fmt.Scanf("%c", &city) // This will read user input and assign to var city
+	fmt.Scan(&city) // This will read user input and assign to var city
 	fmt.Println("What's the name of your pet?")
-	fmt.Scanf("%p", &pet) // This will read user input and assign to var pet
-	fmt.Printf("Your band name could be %c %p.\n")
+	fmt.Scan(&pet) // This will read user input and assign to var pet
+	fmt.Printf("Your band name could be %v %v.\n", city, pet)
 }
+
+/*
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Welcome to Band Name Generator!")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	// Get city name
+	fmt.Print("What's the name of the city you grew up in? ")
+	city, _ := reader.ReadString('\n')
+	city = strings.TrimSpace(city)
+
+	// Get pet name
+	fmt.Print("What's the name of your pet? ")
+	pet, _ := reader.ReadString('\n')
+	pet = strings.TrimSpace(pet)
+
+	// Generate and print band name
+	bandName := fmt.Sprintf("%s %s", city, pet)
+	fmt.Printf("\nYour band name could be: %s! 🎸\n", bandName)
+}
+*/
