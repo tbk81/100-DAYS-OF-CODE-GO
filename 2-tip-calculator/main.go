335f8d09adfea2f87eac4bf749b1bd352e747099
tbk81 <dev@tbk81.com>
2025-01-13 14:29:25 -0800

    day 2

---
+++ 2-tip-calculator/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var bill, tip, people, total float64
+
+	fmt.Print("Welcome to the tip calculator!\nEnter the total cost of the bill: ")
+	fmt.Scanln(&bill)
+
+	fmt.Print("What percentage would you like to tip: ")
+	fmt.Scanln(&tip)
+
+	fmt.Print("How many guests to split the bill: ")
+	fmt.Scanln(&people)
+
+	total = ((bill * (tip / 100)) + bill) / people
+	// trunc := math.Round(total)
+	fmt.Printf("bill: %v\ntip: %v\npeople: %v\ntotal per guest: %.2f\n", bill, tip, people, total)
+}
