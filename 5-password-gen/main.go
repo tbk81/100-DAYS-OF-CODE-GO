ad388aab2b751c9969c1cc5962f4c862ff6e68eb
tbk81 <dev@tbk81.com>
2025-02-10 15:19:19 -0800

    symbol func

--- 5-password-gen/main.go
+++ 5-password-gen/main.go
@@ -9,12 +9,20 @@ func numgen(n int) []int {
 	return rand.Perm(n)
 }
 
+func symgen(n int) []string {
+	return Syms
+}
+
 func main() {
-	var nums int
+	var nums, symbols, letters int
 	// var letters, symbols string
 
 	fmt.Println("Welcome to the GO password generator\nHow many numbers would you like?")
 	fmt.Scanln(&nums)
+	fmt.Println("How many symbols would you like?")
+	fmt.Scanln(&symbols)
+	fmt.Println("How many letters would you like?")
+	fmt.Scanln(&letters)
 
 	final := numgen(nums)
 	fmt.Printf("Num slice:\n%v\nLen of slice:\n%v\n", final, len(final))
