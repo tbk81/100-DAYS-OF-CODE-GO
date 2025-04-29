d0deb2fbac52d6cf2708b44d62aa0a21584ce3eb
tbk81 <dev@tbk81.com>
2025-04-29 05:56:58 -0700

    hangman progress

--- 7-hangman/main.go
+++ 7-hangman/main.go
@@ -6,15 +6,48 @@ import (
 )
 
 func main() {
+	var word []string
+	var currentWord string
 	var currentGuess string
-	word := "hangman"
-	for range len(word) {
-		fmt.Print("_")
-	}
-	fmt.Print("\nGuess a letter: ")
-	fmt.Scanln(&currentGuess)
+	wordToGuess := "hang"
+	lives := len(wordToGuess)
 
-	fmt.Println(strings.Contains(word, currentGuess))
+	// Initializes a slice of _ so index can be referenced and changed
+	for range len(wordToGuess) {
+		word = append(word, "_")
+	}
+	// fmt.Println(word)
+
+	// Prints the values of each index of slice createing a seemless "string"
+	for _, v := range word {
+		fmt.Print(v)
+	}
+
+	for {
+		fmt.Print("\nGuess a letter: ")
+		fmt.Scanln(&currentGuess)
+		// fmt.Println(strings.Contains(wordToGuess, currentGuess))
+		ind := strings.Index(wordToGuess, currentGuess)
+		// fmt.Println("Index:", ind)
+
+		if strings.Contains(wordToGuess, currentGuess) {
+			word[ind] = currentGuess
+		} else {
+			lives -= 1
+		}
+
+		fmt.Println("Lives:", lives)
+		for _, v := range word {
+			currentWord = strings.Join(word, v)
+			fmt.Print(currentWord)
+			// fmt.Print(v)
+		}
+
+		if lives == 0 {
+			fmt.Println("\nGame Over!")
+			break
+		}
+	}
 
 }
 
