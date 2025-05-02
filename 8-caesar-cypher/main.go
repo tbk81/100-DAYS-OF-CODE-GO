3eeb083c3ac6dfa4a4bb3c51320c586ce9fecb80
tbk81 <dev@tbk81.com>
2025-05-02 05:59:29 -0700

    cipher progress

--- 8-caesar-cypher/main.go
+++ 8-caesar-cypher/main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"slices"
+)
+
+var alphabet = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+
+// func Encrypt(word string, shift int) string {
+// 	alphabet :=
+
+// }
+
+// function that shifts the indices of a slice
+func translation(a []string, n int) []string {
+	n = n % len(a)
+	return slices.Concat(a[n:len(a):len(a)]) //, a[:n]...)
+}
+
 func main() {
-	return
+	fmt.Println(translation(alphabet, 26))
+
+	// var encryptWord, usrChoice string
+	// var shiftNumber int
+
+	// fmt.Println("Type 'endcode' to encrypt, tpye 'decode' to decrypt:")
+	// fmt.Scanln(&usrChoice)
+	// if usrChoice == "encode" {
+	// 	fmt.Println("Type your message:")
+	// 	fmt.Scanln(&encryptWord)
+	// 	fmt.Println("Type the shift number:")
+	// 	fmt.Scanln(&shiftNumber)
+	// 	fmt.Print("Here's your encoded result:", Encrypt(encryptWord))
+	// } else if usrChoice == "decode" {
+	// 	fmt.Println("Type your message:")
+	// 	fmt.Scanln(&encryptWord)
+	// } else {
+	// 	fmt.Printf("%v is not an option, try again\n", usrChoice)
+	// }
+
 }
