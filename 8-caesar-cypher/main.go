872450b0e03c2aa768c83514004527b94b320544
tbk81 <dev@tbk81.com>
2025-05-05 06:06:03 -0700

    infinate for loop; switch block

--- 8-caesar-cypher/main.go
+++ 8-caesar-cypher/main.go
@@ -2,43 +2,54 @@ package main
 
 import (
 	"fmt"
-	"slices"
+	"strings"
 )
 
 var alphabet = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
 
-func Encrypt(word string, shift int) string {
-	shift = shift % len(alphabet)
-	shiftSlice := slices.Concat(alphabet[shift:len(alphabet):len(alphabet)])
-
+func findAllIndices(word string) []int {
+	xs := strings.Split(word, "")
+	var xi []int
+	for _, v := range xs {
+		for x, _ := range alphabet {
+			if v == alphabet[x] {
+				xi = append(xi, x)
+			}
+		}
+	}
+	return xi
 }
 
-// function that shifts the indices of a slice
-// func translation(a []string, n int) []string {
-// 	n = n % len(a)
-// 	return slices.Concat(a[n:len(a):len(a)])
-// }
+func Encrypt(word string, shift int) string {
+	shift = shift % len(alphabet)
+	// shiftSlice := slices.Concat(alphabet[shift:len(alphabet):len(alphabet)])
+	return word
+}
 
 func main() {
-	// fmt.Println(translation(alphabet, 26))
-
 	var encryptWord, usrChoice string
 	var shiftNumber int
 
 	fmt.Println("Type 'endcode' to encrypt, tpye 'decode' to decrypt:")
 	fmt.Scanln(&usrChoice)
-	if usrChoice == "encode" {
-		fmt.Println("Type your message:")
-		fmt.Scanln(&encryptWord)
-		fmt.Println("Type the shift number:")
-		fmt.Scanln(&shiftNumber)
-		fmt.Print("Here's your encoded result:", Encrypt(encryptWord))
-	} else if usrChoice == "decode" {
-		fmt.Println("Type your message:")
-		fmt.Scanln(&encryptWord)
-	} else {
-		fmt.Printf("%v is not an option, try again\n", usrChoice)
+Loop:
+	for {
+		switch {
+		case usrChoice == "encode":
+			fmt.Println("Type your message:")
+			fmt.Scanln(&encryptWord)
+			fmt.Println("Type the shift number:")
+			fmt.Scanln(&shiftNumber)
+			fmt.Print("Here's your encoded result:", Encrypt(encryptWord, shiftNumber))
+		case usrChoice == "decode":
+			fmt.Println("Type your message:")
+			fmt.Scanln(&encryptWord)
+		case usrChoice == "done":
+			fmt.Println("Exiting...")
+			break Loop
+		case usrChoice != "encode" && usrChoice != "decode":
+			fmt.Printf("%v is not an option, try again\n", usrChoice)
+			break
+		}
 	}
-
 }
-
