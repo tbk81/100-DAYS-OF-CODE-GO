4fcc34933a70ee34367528e4126a0def030429d9
tbk81 <dev@tbk81.com>
2025-05-07 05:58:20 -0700

    start calulator

---
+++ 10-calculator/main.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+var li = []string{"+", "-", "/", "*"}
+
+func operandDisplay() {
+	for _, v := range li {
+		fmt.Println(v)
+	}
+}
+
+func add(f float64, s float64) float64 {
+	return f + s
+}
+
+func substract(f float64, s float64) float64 {
+	return f - s
+}
+
+func divide(f float64, s float64) float64 {
+	return f / s
+}
+
+func multiply(f float64, s float64) float64 {
+	return f * s
+}
+
+func main() {
+	var firstNum, secondNum, solution float64
+	var operand, usrChoice string
+
+	fmt.Println("Welcome to the 4 function calculator")
+	fmt.Print("Type your first number: ")
+	fmt.Scanln(&firstNum)
+	operandDisplay()
+	fmt.Print("Pick your operation: ")
+	fmt.Scanln(&operand)
+	fmt.Print("Type your second number: ")
+	fmt.Scanln(&secondNum)
+
+	switch {
+	case operand == "+":
+		solution = add(firstNum, secondNum)
+	case operand == "-":
+		solution = substract(firstNum, secondNum)
+	case operand == "/":
+		solution = divide(firstNum, secondNum)
+	case operand == "*":
+		solution = multiply(firstNum, secondNum)
+	}
+
+	fmt.Println(firstNum, operand, secondNum, "=", solution)
+	fmt.Println("Type 'y' to continue calculating or type 'n' to start a new calculation: ")
+	fmt.Scanln(&usrChoice)
+
+}
