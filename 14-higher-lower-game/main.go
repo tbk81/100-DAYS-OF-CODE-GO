0eef2a3a92d6894147f6f929c4e9f736816b5351
tbk81 <dev@tbk81.com>
2025-05-28 05:29:39 -0700

    higher lower

--- 14-higher-lower-game/main.go
+++ 14-higher-lower-game/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"os"
 )
 
 type Person struct {
-	Name           string `json:"name"`
-	Follower_count int    `json:"follower_count"`
-	Description    string `json:"description"`
-	Country        string `json:"country"`
+	Name          string `json:"name"`
+	FollowerCount int    `json:"follower_count"`
+	Description   string `json:"description"`
+	Country       string `json:"country"`
 }
 
 type PeopleData struct {
@@ -18,6 +19,9 @@ type PeopleData struct {
 }
 
 func main() {
+	var input string
+	var score int
+
 	byteValue, err := os.ReadFile("game-data.json")
 	if err != nil {
 		fmt.Println("Error reading file:", err)
@@ -31,8 +35,40 @@ func main() {
 		return
 	}
 
-	for _, person := range people.Data {
-		fmt.Printf("%s from %s has %d followers. Description: %s\n",
-			person.Name, person.Country, person.Follower_count, person.Description)
+	// for _, person := range people.Data {
+	// 	fmt.Printf("%s from %s has %d followers. Description: %s\n",
+	// 		person.Name, person.Country, person.Follower_count, person.Description)
+	// }
+
+	fmt.Println("Welcome to the higher/lower game!")
+Loop:
+	for {
+		a := people.Data[rand.Intn(len(people.Data))]
+		b := people.Data[rand.Intn(len(people.Data))]
+
+		fmt.Printf("\nCompare A: %v, a %v from %v\n", a.Name, a.Description, a.Country)
+		fmt.Println("VS")
+		fmt.Printf("Compare B: %v, a %v from %v\n", b.Name, b.Description, b.Country)
+		fmt.Print("\nWhich account has more followers? Type 'A' or 'B': ")
+		fmt.Scanln(&input)
+		switch input {
+		case "a":
+			if a.FollowerCount > b.FollowerCount {
+				score += 1
+				fmt.Println("You're right! Currnt score:", score)
+			} else {
+				fmt.Println("That's incorrect, game over. Final score: ", score)
+				break Loop
+			}
+		case "b":
+			if b.FollowerCount > a.FollowerCount {
+				score += 1
+				fmt.Println("You're right! Currnt score:", score)
+			} else {
+				fmt.Println("That's incorrect, game over. Final score: ", score)
+				break Loop
+			}
+		}
 	}
+
 }
