15b9bc0abcd7e591e7010c0ea004ea50fcddc457
tbk81 <dev@tbk81.com>
2025-06-06 05:57:48 -0700

    coffee machine

--- 15-coffee-machine/main.go
+++ 15-coffee-machine/main.go
@@ -10,29 +10,41 @@ type Machine struct {
 	Water  int
 	Milk   int
 	Coffee int
-	Cost   float64
-}
-
-type Resources struct {
-	r map[string]Machine
+	Money  float64
 }
 
 type Ingredients struct {
-	Water  int     `json:"water"`
-	Milk   int     `json:"milk"`
-	Coffee int     `json:"coffee"`
-	Cost   float64 `json:"cost"`
+	Water  int `json:"water"`
+	Milk   int `json:"milk"`
+	Coffee int `json:"coffee"`
 }
 
-type Drinks struct {
-	Menu map[string]Ingredients `json:"menu"`
+type Drink struct {
+	Ingredients Ingredients `json:"ingredients"`
+	Cost        float64     `json:"cost"`
+}
+
+type MenuData struct {
+	Menu map[string]Drink `json:"Menu"`
+}
+
+func ResourceCheck(i Ingredients, r Machine) bool {
+	if i.Water > r.Water || i.Milk > r.Milk || i.Coffee > r.Coffee {
+		return false
+	}
+	return true
+}
+
+func DeductResources(i Ingredients, m *Machine, cost float64) {
+	m.Water -= i.Water
+	m.Milk -= i.Milk
+	m.Coffee -= i.Coffee
+	m.Money += cost
 }
 
 func main() {
-
-	var resources Resources
-	var coffee Drinks
-	var usrChoice string
+	var coffee MenuData
+	usrChoice := ""
 
 	byteValue, err := os.ReadFile("menu.json")
 	if err != nil {
@@ -45,30 +57,35 @@ func main() {
 		fmt.Println("Error unmarshaling JSON:", err)
 		return
 	}
-	resources.r["Water"] = 1000
+
+	resources := Machine{
+		Water:  1000,
+		Milk:   500,
+		Coffee: 300,
+		Money:  400.0, // Stored in cents as dollars
+	}
 
 loop:
 	for {
-		fmt.Print("What would you like? (espresso/latte/cappuccino):")
+		fmt.Print("What would you like? (espresso/latte/cappuccino): ")
 		fmt.Scanln(&usrChoice)
 
 		switch usrChoice {
 		case "off":
 			break loop
-		case "espresso":
-			fmt.Println("Your chose espresso")
-		case "latte":
-			fmt.Println("Your chose latte")
-		case "cappuccino":
-			fmt.Println("Your chose cappuccino")
+		case "report":
+			fmt.Printf("Water: %v mL\nMilk: %v mL\nCoffee: %v g\nMoney: $%.2f\n", resources.Water, resources.Milk, resources.Coffee, resources.Money/100)
+		case "espresso", "latte", "cappuccino":
+			drink := coffee.Menu[usrChoice]
+			if ResourceCheck(drink.Ingredients, resources) {
+				DeductResources(drink.Ingredients, &resources, drink.Cost*100)
+				fmt.Printf("You chose %s. It costs $%.2f\n", usrChoice, drink.Cost)
+				fmt.Println("Preparing your drink... Enjoy!")
+			} else {
+				fmt.Println("Sorry, not enough resources to make that drink.")
+			}
+		default:
+			fmt.Println("Invalid choice. Please choose again.")
 		}
 	}
-
-	// for name, drink := range coffee.Menu {
-	// 	fmt.Printf("%v\t%v%T\t%v\t%v\t%v\n", name, drink.Water, drink.Water, drink.Milk, drink.Coffee, drink.Cost)
-	// }
-
-	//fmt.Println("Espresso:", coffee.Menu.Espresso)
-	//fmt.Println("Latte:", coffee.Menu.Latte)
-	//fmt.Println("Cappuccino:", coffee.Menu.Cappuccino)
 }
