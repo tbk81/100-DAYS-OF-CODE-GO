d5701ad44439f8c860850ad5d286514e96667708
tbk81 <dev@tbk81.com>
2025-06-13 05:17:08 -0700

    hirst start

---
+++ 18-turtle-hirst/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	// "image/color"
+	// "github.com/Pitrified/go-turtle"
+	// "github.com/Pitrified/go-turtle/fractal"
+)
+
+func main() {
+	fmt.Println("Hello gopher")
+}
