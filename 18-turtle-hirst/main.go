5826fc79db6a6ac69684947146a77f5dafe3134e
tbk81 <dev@tbk81.com>
2025-06-16 06:04:50 -0700

    hirst

--- 18-turtle-hirst/main.go
+++ 18-turtle-hirst/main.go
@@ -2,11 +2,65 @@ package main
 
 import (
 	"fmt"
-	// "image/color"
-	// "github.com/Pitrified/go-turtle"
-	// "github.com/Pitrified/go-turtle/fractal"
+	"image/color"
+
+	"github.com/Pitrified/go-turtle"
 )
 
+func circle(td *turtle.TurtleDraw) {
+	// move somewhere else
+	td.PenUp()
+	td.SetPos(450, 300)
+
+	// draw a circle with increasing brightness
+	td.PenDown()
+	td.SetHeading(turtle.North)
+	td.SetSize(10)
+	for i := range 360 {
+		val := uint8(float64(i) * 250 / 360)
+		td.SetColor(color.RGBA{val, val / 2, 0, 255})
+		td.Right(1)
+		td.Forward(0.5)
+	}
+}
+
 func main() {
-	fmt.Println("Hello gopher")
+	//w := turtle.NewWorld(900, 600)
+	w := turtle.NewWorldWithColor(900, 900, turtle.White)
+
+	//img := image.NewRGBA(image.Rect(0, 0, 900, 600))
+	//draw.Draw(img, img.Bounds(), &image.Uniform{turtle.Cyan}, image.Point{0, 0}, draw.Src)
+	//wi := turtle.NewWorldWithImage(img)
+
+	// create a turtle attached to w
+	td := turtle.NewTurtleDraw(w)
+
+	// position/orientation
+	td.SetPos(50, 50)
+	td.SetHeading(turtle.East)
+
+	// line style
+	td.SetColor(turtle.Blue)
+	td.SetSize(4)
+
+	// start drawing
+	td.PenDown()
+
+	// same interface as Turtle
+	td.Forward(200)
+	td.Left(90)
+	td.Forward(200)
+
+	circle(td)
+
+	err := w.SaveImage("world.png")
+	if err != nil {
+		fmt.Println("Could not save the image: ", err)
+	}
+
+	w.Close()
+
+	// this is an error: the turtle tries to send the line
+	// to the world input channel that has been closed
+	// td.Forward(50)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tbk81/100-DAYS-OF-CODE-GO
 
 go 1.23.1
+
+require github.com/Pitrified/go-turtle v0.3.0
